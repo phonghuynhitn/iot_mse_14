9a5b28d13cb222173f5bbea1191e628ba34ac9b3
phonghuynh <huynhtranvuquocphong@gmail.com>
2024-03-13 18:59:19 +0700

    edit

--- day08/rs485.py
+++ day08/rs485.py
@@ -19,6 +19,19 @@ def getPort():
 portName = "/dev/ttyUSB0"
 print(portName)
 
+def serial_read_data(ser):
+    bytesToRead = ser.inWaiting()
+    if bytesToRead > 0:
+        out = ser.read(bytesToRead)
+        data_array = [b for b in out]
+        print(data_array)
+        if len(data_array) >= 7:
+            array_size = len(data_array)
+            value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
+            return value
+        else:
+            return -1
+    return 0
 
 
 try:
@@ -45,19 +58,7 @@ while True:
     time.sleep(2)
 
 
-def serial_read_data(ser):
-    bytesToRead = ser.inWaiting()
-    if bytesToRead > 0:
-        out = ser.read(bytesToRead)
-        data_array = [b for b in out]
-        print(data_array)
-        if len(data_array) >= 7:
-            array_size = len(data_array)
-            value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
-            return value
-        else:
-            return -1
-    return 0
+
 
 soil_temperature =[1, 3, 0, 6, 0, 1, 100, 11]
 def readTemperature():
