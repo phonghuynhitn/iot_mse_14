8640d9b63937d12119f53a86f45a57b32b98898f
phonghuynh <huynhtranvuquocphong@gmail.com>
2024-03-13 20:11:10 +0700

    test

--- day08/rs485.py
+++ day08/rs485.py
@@ -43,8 +43,11 @@ try:
 except:
     print("Can not open the port")
 
-relay1_ON  = [2, 6, 0, 0, 0, 255, 200, 91]
-relay1_OFF = [2, 6, 0, 0, 0, 0, 136, 27]
+# relay1_ON  = [2, 6, 0, 0, 0, 255, 200, 91]
+# relay1_OFF = [2, 6, 0, 0, 0, 0, 136, 27]
+    
+relay1_ON  = [2, 6, 0, 0, 0, 255, 222, 95]
+relay1_OFF = [2, 6, 0, 0, 0, 0, 146, 37]
 
 def setDevice1(state):
     if state == True:
