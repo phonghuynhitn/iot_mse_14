b776f026cfbe22dc214ef56eb3e7e6bec63eaae9
pi <pi@gmail.com>
2024-03-21 22:41:25 +0700

    set relay

--- project/main.py
+++ project/main.py
@@ -12,6 +12,11 @@ MQTT_TOPIC_SUB = [f"{MQTT_USERNAME}/feeds/relay02", f"{MQTT_USERNAME}/feeds/rela
 
 temperature_index = 0
 moisture_index = 1
+
+relay02_index = 0
+relay03_index = 1
+relay04_index = 2
+
 sensors_and_actuators = SensorsAndActuators()
 
 def mqtt_connected(client, userdata, flags, rc):
@@ -25,6 +30,24 @@ def mqtt_subscribed(client, userdata, mid, granted_qos):
 
 def message(client , feed_id , payload):
     print("Recieved: " + str(payload.payload.decode('utf-8')))
+    data = str(payload.payload.decode('utf-8'))
+    if feed_id == MQTT_TOPIC_SUB[relay02_index]:
+        if data == '1':
+            sensors_and_actuators.set_relay(2, True)
+        elif data == '0':
+            sensors_and_actuators.set_relay(2, False)
+    if feed_id == MQTT_TOPIC_SUB[relay03_index]:
+        if data == '1':
+            sensors_and_actuators.set_relay(3, True)
+        elif data == '0':
+            sensors_and_actuators.set_relay(3, False)
+    if feed_id == MQTT_TOPIC_SUB[relay04_index]:
+        if data == '1':
+            sensors_and_actuators.set_relay(4, True)
+        elif data == '0':
+            sensors_and_actuators.set_relay(4, False)
+        
+            
 
 mqttClient = mqtt.Client()
 mqttClient.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
@@ -41,10 +64,10 @@ while True:
     temperature = sensors_and_actuators.read_temperature()
     print(f"Nhiet do:", temperature)
     # if temperature > 0:
-    mqttClient.publish(MQTT_TOPIC_PUB[temperature_index], temperature)
+    mqttClient.publish(MQTT_TOPIC_PUB[temperature_index], temperature, retain=True)
 
     moisture = sensors_and_actuators.read_moisture()
     print(f"Do am:", moisture)
     # if moisture > 0:
-    mqttClient.publish(MQTT_TOPIC_PUB[moisture_index], moisture)
+    mqttClient.publish(MQTT_TOPIC_PUB[moisture_index], moisture, retain=True)
     time.sleep(1)
