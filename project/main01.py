e8d15903a64c4c0de485f92c9653904f9cee2d79
pi <pi@gmail.com>
2024-03-24 20:49:59 +0700

    update

---
+++ project/main01.py
@@ -0,0 +1,90 @@
+#  pip install paho-mqtt==1.6.1
+import paho.mqtt.client as mqtt
+import time
+from sensors_and_actuators import *
+from model import *
+
+MQTT_SERVER = "mqtt.ohstem.vn"
+MQTT_PORT = 1883
+MQTT_USERNAME = "mse14-group2"
+MQTT_PASSWORD = "1234"
+MQTT_TOPIC_PUB = [f"{MQTT_USERNAME}/feeds/temperature", f"{MQTT_USERNAME}/feeds/moisture"]
+MQTT_TOPIC_SUB = [f"{MQTT_USERNAME}/feeds/relay02", f"{MQTT_USERNAME}/feeds/relay03", f"{MQTT_USERNAME}/feeds/relay04"]
+
+temperature_index = 0
+moisture_index = 1
+
+relay02_index = 0
+relay03_index = 1
+relay04_index = 2
+
+sensors_and_actuators = SensorsAndActuators()
+file_path = "temperature_data.txt"
+predictor = TemperaturePredictor(file_path)
+
+def mqtt_connected(client, userdata, flags, rc):
+    for feed in MQTT_TOPIC_SUB:
+        client.subscribe(feed)
+    print("Connected succesfully!!")
+    
+
+def mqtt_subscribed(client, userdata, mid, granted_qos):
+    print("Subscribed to Topic!!!")
+
+def message(client , feed_id , payload):
+    print("Recieved: " + str(payload.payload.decode('utf-8')))
+    data = str(payload.payload.decode('utf-8'))
+    if feed_id == MQTT_TOPIC_SUB[relay02_index]:
+        if data == '1':
+            sensors_and_actuators.set_relay(2, True)
+        elif data == '0':
+            sensors_and_actuators.set_relay(2, False)
+    if feed_id == MQTT_TOPIC_SUB[relay03_index]:
+        if data == '1':
+            sensors_and_actuators.set_relay(3, True)
+        elif data == '0':
+            sensors_and_actuators.set_relay(3, False)
+    if feed_id == MQTT_TOPIC_SUB[relay04_index]:
+        if data == '1':
+            sensors_and_actuators.set_relay(4, True)
+        elif data == '0':
+            sensors_and_actuators.set_relay(4, False)
+        
+            
+
+mqttClient = mqtt.Client()
+mqttClient.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+mqttClient.connect(MQTT_SERVER, int(MQTT_PORT), 60)
+
+#Register mqtt events
+mqttClient.on_connect = mqtt_connected
+mqttClient.on_subscribe = mqtt_subscribed
+mqttClient.on_message =  message
+mqttClient.loop_start()
+
+counter = 60
+while True:
+    temperature = sensors_and_actuators.read_temperature()
+    print(f"Nhiet do:", temperature)
+    counter = counter - 1
+    if(counter <= 0):
+        if temperature is not None:
+            print("Nhiệt độ: {:.1f}°C".format(temperature))
+
+            predictor.write_temperature_to_file(temperature)
+
+            predictor.train_model()
+
+            predicted_temperature = predictor.predict_next_temperature()
+            print("Dự đoán nhiệt độ cho phút tới: {:.1f}°C".format(predicted_temperature))
+        else:
+            print("Không thể đọc dữ liệu từ cảm biến. Thử lại sau.")
+        counter = 60
+    # if temperature > 0:
+    mqttClient.publish(MQTT_TOPIC_PUB[temperature_index], temperature, retain=True)
+
+    moisture = sensors_and_actuators.read_moisture()
+    print(f"Do am:", moisture)
+    # if moisture > 0:
+    mqttClient.publish(MQTT_TOPIC_PUB[moisture_index], moisture, retain=True)
+    time.sleep(1)
---
+++ project/model.py
@@ -0,0 +1,60 @@
+import time
+import datetime
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import random
+
+class TemperaturePredictor:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.model = LinearRegression()
+
+    def write_temperature_to_file(self, temperature):
+        with open(self.file_path, "a") as file:
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            file.write(f"{timestamp}, {temperature}\n")
+
+    def read_temperature_data(self):
+        timestamps = []
+        temperatures = []
+        with open(self.file_path, "r") as file:
+            for line in file:
+                timestamp, temperature = line.strip().split(", ")
+                timestamps.append(datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S"))
+                temperatures.append(float(temperature))
+        return timestamps, temperatures
+
+    def train_model(self):
+        timestamps, temperatures = self.read_temperature_data()
+        X_train, X_test, y_train, y_test = train_test_split(np.array(range(len(timestamps))).reshape(-1, 1), temperatures, test_size=0.2, random_state=42)
+        self.model.fit(X_train, y_train)
+
+    def predict_next_temperature(self):
+        timestamps, temperatures = self.read_temperature_data()
+        next_minute = len(temperatures) + 1
+        predicted_temperature = self.model.predict([[next_minute]])
+        return predicted_temperature[0]
+
+    def simulate_temperature_measurement(self):
+        while True:
+            temperature = random.randint(20, 40)
+
+            if temperature is not None:
+                print("Nhiệt độ: {:.1f}°C".format(temperature))
+
+                self.write_temperature_to_file(temperature)
+
+                self.train_model()
+
+                predicted_temperature = self.predict_next_temperature()
+                print("Dự đoán nhiệt độ cho phút {}: {:.1f}°C".format(next_minute, predicted_temperature))
+            else:
+                print("Không thể đọc dữ liệu từ cảm biến. Thử lại sau.")
+
+            time.sleep(60)
+
+if __name__ == "__main__":
+    file_path = "temperature_data.txt"
+    predictor = TemperaturePredictor(file_path)
+    predictor.simulate_temperature_measurement()
